ab9b57229b6785197e237970b228e5f514094b52
Toky_22201559 <127178543+mrbruce26@users.noreply.github.com>
2024-11-08 14:32:31 +0600

    Add files via upload
    Suppose you are given a task to rank the students. You have gotten the marks and ID of
    the students. Now your task is to rank the students based on their marks using a sorting
    algorithm.
    However, you have to keep in mind that your sorting algorithms perform the
    minimum number of swapping operations.
    
    Input:
    The first line of the input file will contain an integer N ( 1 ≤ N ≤ 1000 ).
    The second line will contain N integers, representing the Student ID, Si
    
    ( 1 ≤ Si ≤ 1000 ).
    The next line will contain the N integer, Sm ( 1 ≤ Sm ≤ 1000 ), which denotes the
    obtained mark of the corresponding students. Suppose you are given a task to rank the
    students. You have gotten the marks and ID of the students. Now your task is to rank
    the students based on their marks using a sorting algorithm.
    However, you have to keep in mind that your sorting algorithms perform the minimum
    number of swapping operations.
    Input:
    The first line of the input file will contain an integer N ( 1 ≤ N ≤ 1000 ).
    The second line will contain N integers, representing the Student ID, Si ( 1 ≤ Si ≤ 1000 ).
    The next line will contain the N integer, Sm ( 1 ≤ Sm ≤ 1000 ), which denotes the
    obtained mark of the corresponding students.
    Output:
    You have to show the Student ID and obtained marks in descending order based on
    their obtained mark. If two or more students get the same mark, then students with the
    lower ID will get prioritized. See the input and output for a better understanding.
    Input 1:
    7
    7 4 9 3 2 5 1
    40 50 50 20 10 10 10
    Output 1:
    ID: 4 Mark: 50
    ID: 9 Mark: 50
    
    ID: 7 Mark: 40
    ID: 3 Mark: 20
    ID: 1 Mark: 10
    ID: 2 Mark: 10
    ID: 5 Mark: 10
    Input 2:
    4
    7 2 5 3
    80 60 80 50
    Output 2:
    ID: 5 Mark: 80
    ID: 7 Mark: 80
    ID: 2 Mark: 60
    ID: 3 Mark: 50

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-inp = open("input1b.txt","r")
-out = open("output1b.txt","w")
-a = inp.readline()
-out.write(a)
-z = inp.read()
-y = z.split("\n")
-for i in y:
-    h = i.split(" ")
-    if(h[2]=="+"):
-        out.write(f"The result of {(h[1])} + {h[3]} is {int(h[1])+int(h[3])}\n")
-    elif (h[2] == "*"):
-        out.write(f"The result of {(h[1])} * {h[3]} is {int(h[1]) * int(h[3])}\n")
-    elif (h[2] == "-"):
-        out.write(f"The result of {(h[1])} - {h[3]} is {int(h[1]) - int(h[3])}\n")
-    elif (h[2] == "/"):
-        out.write(f"The result of {(h[1])} / {h[3]} is {int(h[1]) / int(h[3])}\n")
+inp = open("input.txt","r")
+out = open("output.txt","w")
+inp.readline()
+b = inp.readline().split(" ")
+c = inp.readline().split(" ")
+print(b)
+print(c)
+l1=[]
+l2 =[]
+for i in range(len(b)):
+    l1.append(int(b[i]))
+    l2.append(int(c[i]))
+print(l1)
+
+min_i=0
+for i in range(len(l1)):
+    min_i = i
+    for j in range(i+1,len(l1)):
+        if l2[min_i] < l2[j]:
+            min_i =j
+        elif l2[min_i]==l2[j]:
+            if l1[min_i]>l1[j]:
+                min_i =j
+    temp= l1[i]
+    tem = l2[i]
+    l1[i] = l1[min_i]
+    l2[i] = l2[min_i]
+    l1[min_i]=temp
+    l2[min_i] = tem
+    out.write(f"ID: {l1[i]} Mark: {l2[i]}\n")
+print(l1)
+print(l2)
